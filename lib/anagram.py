f94ca1ea304ba82c1d5a535ef9cd57a777a1069c
Joseph-Hansel <abogejosephhansel@gmail.com>
2025-08-27 11:55:07 +0300

    Finish Assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self,word):
+        self.word = word
+
+    def match(self,list):
+        matched = []
+        for w in list:
+            test_letters = [l for l in w]
+            template_letters = [l for l in self.word]
+            if sorted(test_letters) == sorted(template_letters):
+                matched.append(w)
+        return matched
+
